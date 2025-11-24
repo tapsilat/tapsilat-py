24abb372087ce1f486a6eb36bc239c3c497b0f56
Atakan Argın <atakan@monopayments.com>
2025-11-24 18:02:42 +0300

    version: updated to v2025.11.24.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="tapsilat-py",
-    version="2025.11.24.1",
+    version="2025.11.24.2",
     description="Client SDK for Tapsilat API",
     long_description=long_description,
     long_description_content_type="text/markdown",

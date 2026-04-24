b7af99534cc49dbad0e649debe35f7836480d106
Hüseyin Mert <huseyinin@gmail.com>
2026-04-24 23:08:42 +0300

    Bump version from 2026.4.24.1 to 2026.4.24.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="tapsilat-py",
-    version="2026.4.24.1",
+    version="2026.4.24.2",
     description="Client SDK for Tapsilat API",
     long_description=long_description,
     long_description_content_type="text/markdown",

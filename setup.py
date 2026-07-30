1c313517e648072e7e5ee527e04cea9424983803
Atakan Argın <atakan@monopayments.com>
2026-07-30 17:53:07 +0300

    bump: version to 2026.7.30.1

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="tapsilat-py",
-    version="2026.7.7.1",
+    version="2026.7.30.1",
     description="Client SDK for Tapsilat API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Tapsilat",
-    author_email="support@tapsilat.dev",
+    author_email="[EMAIL_ADDRESS]",
     url="https://github.com/tapsilat/tapsilat-py",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
